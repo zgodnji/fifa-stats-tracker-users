10cb4e58edb43191288a1cdb6263abae1b15b73d
Andraz Krasovec <ak6688@student.uni-lj.si>
2017-11-28 12:24:29 +0000

    Service discovery added.

--- src/main/java/com/zgodnji/fifastattracker/UserApplication.java
+++ src/main/java/com/zgodnji/fifastattracker/UserApplication.java
@@ -1,9 +1,11 @@
 package com.zgodnji.fifastattracker;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-
+@RegisterService
 @ApplicationPath("v1")
 public class UserApplication extends Application {
 }
